b6d481c5db7841e0281c840358116574a2a4909a
Nestor Padilla <pcu18021379@gmail.com>
2022-10-19 22:01:43 -0600

    Input para agregar tarea, agregado.

--- pro_todoList.js
+++ pro_todoList.js
@@ -1,3 +1,10 @@
 /*Proyecto donde el usuario puede ingresar una lista de deberes, y después marcar un elemento
 de la lista que ya haya realizado*/
 
+
+//Funcion que guarda el id de la tarea en una variables y hace que aparezca en el documento
+function Add(){
+    const task = document.getElementById("tarea");
+    task.style.display = "block";
+    return true;
+}
