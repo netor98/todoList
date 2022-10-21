71f5bf022162510d3372f7ce5bff2480a51be14d
Nestor Padilla <pcu18021379@gmail.com>
2022-10-20 23:15:48 -0600

    Agrenado las tareas al main div, y vector que cambia a fondo oscuro

--- pro_todoList.js
+++ pro_todoList.js
@@ -8,3 +8,32 @@ function Add(){
     task.style.display = "block";
     return true;
 }
+
+function dark(){
+    const changecolor = document.getElementById("fondo");
+    changecolor.style.backgroundColor = "#191919";
+    return true;
+}
+
+const task = document.getElementById("tarea");
+//document.addEventListener('keyup', showTask(event));
+
+/*function showTask(e){
+    if(event.code === "Enter"){
+        const mainDiv = document.getElementById("cuadro_principal");
+        const element = document.getElementById("tarea").value;
+        mainDiv+=`${e.code}`;
+    }   
+}*/
+
+document.addEventListener("keyup", function(event) {
+    if (event.code === 'Enter') {
+        let mainDiv = document.getElementById("cuadro_principal");
+        let element = document.getElementById("tarea").value;
+        let content = document.createTextNode(element);
+        console.log(element);
+        mainDiv.appendChild(content);
+    }
+});
+
+
